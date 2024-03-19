10d3c290f83aeb2ca0cfba823d8a01e936a2e7bb
filipaldi <piano.haggle.0d@icloud.com>
2024-03-19 13:30:24 +0100

    actualise parser

--- amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
@@ -1,4 +1,12 @@
-# svg_import_parser.py
+'''
+svg_import_parser.py
+
+For `Selective Import`, bind the parsing data method and print the parsed data.
+- refactor the code of involved files
+- keep parser logic in `svg_import_parser.py` so that it handles only parsing regardless of selective or batch import, as `svg_import_batcher.py` always provides only one file at the batch.
+- keep main logic in `svg_import.py`
+
+
 import xml.etree.ElementTree as ET
 
 def parse_svg(svg_path):
@@ -17,3 +25,17 @@ def parse_svg(svg_path):
     except ET.ParseError:
         print(f"Error parsing SVG file: {svg_path}")
         return None
+
+def parse_single_svg(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        paths = [path.attrib['d'] for path in root.findall('.//{http://www.w3.org/2000/svg}path')]
+        glyph_name = file_path.split('/')[-1].replace('_refined.svg', '')
+        print(f"Parsed data for {glyph_name}: {paths}")
+        return {'glyph_name': glyph_name, 'paths': paths}
+    except Exception as e:
+        print(f"Error parsing SVG file {file_path}: {e}")
+        return None
+
+'''
