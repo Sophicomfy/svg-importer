7fce3b6fcc7a6290fa3143bd8e3c5139ac610f15
filipaldi <piano.haggle.0d@icloud.com>
2024-03-19 10:27:42 +0100

    remove old code

--- old_code.glyphsPlugin/Contents/Resources/svg_import_distributor.py
+++
@@ -1,8 +0,0 @@
-""""
-## Next goal
-Distribute parsed data into glyphs within the glyphs file.
-
-Code `svg_import_distributor.py`
-Provide code for dependent files
-Remember to follow "coding workflow"
-""""
