2eb2c7de26a14483438f410b8564239eb05330b2
filipaldi <piano.haggle.0d@icloud.com>
2024-03-22 18:13:41 +0100

    parser simplified

--- amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
@@ -10,12 +10,7 @@ def parse_svg(svg_path, print_parsed_data=False):
         glyph_name = svg_path.split('/')[-1].replace('_refined.svg', '')
         parsed_data = {'glyph_name': glyph_name, 'paths': path_data}
 
-        if print_parsed_data:
-            print(f"Glyph Name: {parsed_data['glyph_name']}")
-            for i, path in enumerate(parsed_data['paths']):
-                print(f"  Path {i+1}: {path}")
-
-        print("Forwarding the following data to svg_import_converter.py:")
+        print("Parsed data: ")
         print(parsed_data)
 
         return parsed_data
