4f8f34cca2049b799d7ca55ab6b80c0238019356
filipaldi <piano.haggle.0d@icloud.com>
2024-03-24 12:42:13 +0100

    update converted

--- amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
@@ -1,7 +1,6 @@
 from GlyphsApp import GSNode, GSLINE, GSCURVE, GSOFFCURVE
 
-def parse_svg_path(svg_path):
-    # Initialize an empty list to store nodes for a single path
+def convert_svg_path_to_glyphs_nodes(svg_path):
     nodes = []
     commands = svg_path.split()
     i = 0
@@ -9,47 +8,47 @@ def parse_svg_path(svg_path):
     while i < len(commands):
         command = commands[i]
         if command in ['M', 'L']:
-            # Handle 'move to' and 'line to' commands
             x, y = float(commands[i+1]), float(commands[i+2])
-            # Add a line node to the nodes list
-            nodes.append({"x": x, "y": y, "type": "line"})
+            # Convert nodes to the tuple format (x, y, type)
+            nodes.append((x, y, 'line'))
             i += 3
         elif command == 'C':
-            # Handle cubic Bezier curve command
             cp1 = (float(commands[i+1]), float(commands[i+2]))
             cp2 = (float(commands[i+3]), float(commands[i+4]))
             end = (float(commands[i+5]), float(commands[i+6]))
-            # Add off-curve control points and the curve node to the nodes list
+            # Convert nodes to the tuple format including offcurve and curve types
             nodes.extend([
-                {"x": cp1[0], "y": cp1[1], "type": "offcurve"},
-                {"x": cp2[0], "y": cp2[1], "type": "offcurve"},
-                {"x": end[0], "y": end[1], "type": "curve"}
+                (cp1[0], cp1[1], 'offcurve'),
+                (cp2[0], cp2[1], 'offcurve'),
+                (end[0], end[1], 'curve')
             ])
             i += 7
         # Handle other commands as necessary
-
+    print("Converted nodes")
+    print(nodes)
     return nodes
 
-def convert_svg_path_to_glyphs_nodes(parsed_data):
-    if isinstance(parsed_data, dict):
-        parsed_data = [parsed_data]
-    
+
+def construct_glyphs_data_structure(converted_data):
+    if isinstance(converted_data, dict):
+        converted_data = [converted_data]
+
     converted_data = []
 
-    for glyph_data in parsed_data:
+    for glyph_data in converted_data:
         glyph_name = glyph_data.get('glyph_name', 'Unknown')
         svg_paths = glyph_data.get('paths', [])
-
         glyphs_nodes = []
 
         for path in svg_paths:
-            nodes = parse_svg_path(path)
+            nodes = convert_svg_path_to_glyphs_nodes(path)
             glyphs_nodes.append({"closed": True, "nodes": nodes})
 
-        converted_data.append({
+        construct_glyph = {
             "glyphname": glyph_name,
-            "layers": [{"shapes": glyphs_nodes}]
-        })
+            "layers": [{"shapes": [{"closed": 1, "nodes": glyphs_nodes}]}]
+        }
+        converted_data.append(construct_glyph)
 
     # Directly print the converted data to verify the output
     print(converted_data)
