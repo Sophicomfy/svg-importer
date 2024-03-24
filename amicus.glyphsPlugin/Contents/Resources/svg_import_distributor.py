3241e9004b4ffee700059524cc5fc10a6b52574d
filipaldi <piano.haggle.0d@icloud.com>
2024-03-24 14:29:37 +0100

    distributor first iteration

--- amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
@@ -1,33 +1,38 @@
-from GlyphsApp import Glyphs
+from GlyphsApp import Glyphs, GSGlyph, GSLayer, GSPath, GSNode
 
 def distribute_converted_data(converted_data):
     font = Glyphs.font  # Current font opened in Glyphs
 
     for data_item in converted_data:
-        try:
-            glyph_name = data_item["glyphname"]
-            glyph = font.glyphs[glyph_name]
+        glyph_name = data_item["glyphname"]
+        glyph = font.glyphs[glyph_name]
 
-            if not glyph:
-                glyph = GSGlyph(glyph_name)
-                font.glyphs.append(glyph)
-            
-            new_layer = GSLayer()
-            new_layer.layerId = data_item["layers"][0]["layerId"]
-            new_layer.width = data_item["layers"][0]["width"]
-            glyph.layers.append(new_layer)
+        # Check if glyph exists, if not, create it
+        if not glyph:
+            glyph = GSGlyph(glyph_name)
+            font.glyphs.append(glyph)
 
-            for shape in data_item["layers"][0]["shapes"]:
-                new_path = GSPath()
-                for node_data in shape["nodes"]:
-                    x, y, node_type = node_data
-                    new_node = GSNode((x, y), type=node_type)
-                    new_path.nodes.append(new_node)
-                new_path.closed = shape["closed"]
-                new_layer.paths.append(new_path)
+        # Create new layer for the glyph
+        new_layer = GSLayer()
+        new_layer.layerId = data_item["layers"][0]["layerId"]
+        new_layer.width = data_item["layers"][0]["width"]
+        glyph.layers.append(new_layer)
 
+        # Draw paths from the converted data into the new layer
+        for shape in data_item["layers"][0]["shapes"]:
+            new_path = GSPath()
+            for node_data in shape["nodes"]:
+                x, y, node_type = node_data
+                node_type = 'LINE' if node_type == 'l' else 'CURVE' if node_type == 'c' else 'OFFCURVE'
+                new_node = GSNode(NSPoint(x, y), type=node_type)
+                new_path.nodes.append(new_node)
+            new_path.closed = True if shape["closed"] == 1 else False
+            new_layer.paths.append(new_path)
+
+        if "unicode" in data_item:
             glyph.unicode = data_item["unicode"]
-        except TypeError as e:
-            print(f"Error processing glyph {glyph_name}: {e}")
 
     font.save()
+
+
+distribute_converted_data(converted_data)
