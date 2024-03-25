54ba56ea47397f12f58aeeadf3e3fccc7e0ba6b9
filipaldi <piano.haggle.0d@icloud.com>
2024-03-25 17:59:59 +0100

    Path scaling

--- amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
@@ -6,29 +6,30 @@ NODE_TYPES = {
     'C': 'CURVE',  # Curveto requires offcurve and a curve node in Glyphs
 }
 
+
 def convert_svg_command_to_glyphs_node_type(command):
-    return NODE_TYPES.get(command, 'OFFCURVE')
+    return NODE_TYPES.get(command, 'o')
 
 def convert_svg_path_to_glyphs_nodes(svg_path):
     nodes = []
     commands = svg_path.split()
+    path_scale = 100
+
     i = 0
 
     while i < len(commands):
         command = commands[i]
         if command in ['M', 'L']:
-            # Round x and y values to nearest integer after converting to float
-            x, y = round(float(commands[i+1]) * 100), round(float(commands[i+2]) * 100)
-            # Append node data without spaces and without quotes around the node type
+            x, y = round(float(commands[i+1]) * path_scale), round(float(commands[i+2]) * path_scale)
             nodes.append(f"({x},{y},l)")  # Using 'l' for line nodes
             i += 3
         elif command == 'C':
-            # For control points and the endpoint, round values and format accordingly
-            cp1 = f"({round(float(commands[i+1]) * 10)},{round(float(commands[i+2]) * 10)},o)"
-            cp2 = f"({round(float(commands[i+3]) * 10)},{round(float(commands[i+4]) * 10)},o)"
-            end = f"({round(float(commands[i+5]) * 10)},{round(float(commands[i+6]) * 10)},c)" 
+            cp1 = f"({round(float(commands[i+1]) * path_scale)},{round(float(commands[i+2]) * path_scale)},o)"
+            cp2 = f"({round(float(commands[i+3]) * path_scale)},{round(float(commands[i+4]) * path_scale)},o)"
+            end = f"({round(float(commands[i+5]) * path_scale)},{round(float(commands[i+6]) * path_scale)},c)" 
             nodes.extend([cp1, cp2, end])
             i += 7
+    print(nodes)
     return nodes
 
 def construct_glyphs_data_structure(parsed_data):
