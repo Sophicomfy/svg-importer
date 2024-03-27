a5176c795309c2d2dde2373e1d9fdb08043ddc2e
filipaldi <piano.haggle.0d@icloud.com>
2024-03-27 09:40:48 +0100

    distributor refactored

--- amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
@@ -1,37 +1,29 @@
-from GlyphsApp import Glyphs, GSGlyph, GSLayer, GSNode, GSPath
-from AppKit import NSPoint
+from GlyphsApp import Glyphs, GSLayer
 
-def create_unique_layer_name(glyph, base_name="Amicus"):
-    existing_layer_names = [layer.name for layer in glyph.layers]
-    if base_name not in existing_layer_names:
-        return base_name
-    else:
-        i = 2
-        while f"{base_name} {i}" in existing_layer_names:
-            i += 1
-        return f"{base_name} {i}"
-
-def distribute_converted_data(converted_data):
-    font = Glyphs.font  # Access the current font
-    for data_item in converted_data:
-        glyph_name = data_item['glyphname']
+def distribute_to_glyphs(glyph_name, shapes_data):
+    font = Glyphs.font
+    
+    glyph = font.glyphs[glyph_name]
+    if not glyph:
+        glyph = GSGlyph(glyph_name)
+        font.glyphs.append(glyph)
         glyph = font.glyphs[glyph_name]
+    
+    import_layers = [layer for layer in glyph.layers if layer.name.startswith("import")]
+    if import_layers:
+        highest_number = max([int(layer.name.replace("import", "")) for layer in import_layers])
+        new_layer_number = highest_number + 1
+    else:
+        new_layer_number = 1
 
-        if not glyph:
-            glyph = GSGlyph(glyph_name)
-            font.glyphs.append(glyph)
+    new_layer_name = "import" + str(new_layer_number).zfill(2)
+    new_layer = GSLayer()
+    new_layer.name = new_layer_name
+    glyph.layers.append(new_layer)
 
-        new_layer_name = create_unique_layer_name(glyph, "Amicus")
-        new_layer = GSLayer()
-        new_layer.name = new_layer_name
-        glyph.layers.append(new_layer)
-
-        # Here, directly insert shapes data as it is
-        shapes_data = data_item['shapes']  # Assuming 'shapes' is a key in your data_item dictionary
-        # This is the crucial part, directly inserting shapes data into the newly created layer's userData
-        new_layer.userData['importedShapes'] = shapes_data
+    new_layer.shapes = eval(shapes_data)  
 
     font.save()
 
+distribute_to_glyphs(glyph_name, shapes_data)
 
-    
