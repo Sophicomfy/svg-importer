f5086f4fccaf9cc75a3cf7530430bb8f0c755a40
filipaldi <piano.haggle.0d@icloud.com>
2024-03-30 13:24:43 +0100

    🥳 svg_import_distributor.py creates glyphs and layers as expected
    1. Code updated → Plugin reinstalled → Glyphs restarted
    2. Run `Selective Import` → new glyphs and layers created
    3. Run `Batch Import`→ new glyphs and layers created

--- amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_distributor.py
@@ -1,30 +1,23 @@
 import GlyphsApp
 from GlyphsApp import Glyphs, GSGlyph
 from svg_import_converter import convert_svg_to_glyphs_layer
-from svg_import_parser import parse_glyph_name
+from svg_import_parser import parse_glyph_name, parse_layer_name
 
 def distribute_data(svg_file_path):
     font = Glyphs.font  # Access the current font
-    glyph_name = parse_glyph_name(svg_file_path)  # Correctly parse the glyph name from the SVG file
+    glyph_name = parse_glyph_name(svg_file_path)  # Parse the glyph name
+    layer_name = parse_layer_name(svg_file_path)  # Parse the layer name
     
-    # Find or create glyph
     glyph = font.glyphs[glyph_name]
     if not glyph:
-        glyph = GlyphsApp.GSGlyph(glyph_name)
+        glyph = GSGlyph(glyph_name)
         glyph.name = glyph_name
         font.glyphs.append(glyph)
-        glyph = font.glyphs[glyph_name]
     
-    # Convert SVG file to a Glyphs layer
     new_layer = convert_svg_to_glyphs_layer(svg_file_path)
     if new_layer:
-        # Name the new layer appropriately
-        iteration_number = len(glyph.layers)
-        new_layer.name = f"imported_svg_{iteration_number}"
+        new_layer.name = f"{layer_name}"  # Apply the parsed layer name directly
         glyph.layers.append(new_layer)
-        print(f"SVG from {svg_file_path} has been imported into glyph '{glyph_name}' with layer named '{new_layer.name}'.")
+        print(f"SVG from {svg_file_path} imported into '{glyph_name}' with layer named '{new_layer.name}'.")
     else:
-        print(f"Failed to convert SVG file: {svg_file_path}")
-
-        print(f"Failed to convert SVG file: {svg_file_path}")
-
+        print(f"Failed to convert SVG: {svg_file_path}")
