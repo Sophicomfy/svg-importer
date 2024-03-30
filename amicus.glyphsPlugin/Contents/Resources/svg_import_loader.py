76ed380e7709424e3b5fc35ec711bdb6e2dd0e76
filipaldi <piano.haggle.0d@icloud.com>
2024-03-30 11:46:31 +0100

    💡 add filter for `_refined.svg` so the loader import only correct files

--- amicus.glyphsPlugin/Contents/Resources/svg_import_loader.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_loader.py
@@ -16,7 +16,7 @@ class SVGImportLoader:
     def load_svg_files_from_folder(self, folder_path):
         svg_files = []
         for file_name in os.listdir(folder_path):
-            if file_name.endswith(".svg"):
+            if file_name.endswith("_refined.svg"): # we need _refined otherwise importing bullshits
                 full_path = os.path.join(folder_path, file_name)
                 svg_files.append(full_path)
                 print(f"Loaded SVG file: {full_path}")
