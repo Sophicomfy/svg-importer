6847ac3ef226c237e9b330d8688f84a5a4930159
filipaldi <piano.haggle.0d@icloud.com>
2024-03-30 09:45:25 +0100

    🐞

--- amicus.glyphsPlugin/Contents/Resources/svg_import_batcher.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_batcher.py
@@ -7,8 +7,8 @@ def batch_process_svgs(folder_path):
     loader = SVGImportLoader()
     svg_files = loader.load_svg_files_from_folder(folder_path)
     for svg_file in svg_files:
-        layer = convert_svg_to_glyphs_path(svg_file)
-        glyph_name = parse_glyph_name(svg_file)  # Using the existing parsing logic
+        layer = convert_svg_to_glyphs_layer(svg_file)
+        glyph_name = parse_glyph_name(svg_file)
 
         glyph = Glyphs.font.glyphs[glyph_name]
         if not glyph:
