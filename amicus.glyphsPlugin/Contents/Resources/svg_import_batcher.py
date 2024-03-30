20745d86b0f94a03895593da61bb4134c2b560d2
filipaldi <piano.haggle.0d@icloud.com>
2024-03-30 11:16:41 +0100

    my correction of import

--- amicus.glyphsPlugin/Contents/Resources/svg_import_batcher.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_batcher.py
@@ -4,7 +4,8 @@ from svg_import_converter import convert_svg_to_glyphs_layer
 from svg_import_distributor import distribute_data
 
 def batch_process_svgs(folder_path):
-    svg_files = load_svg_files_from_folder(folder_path)  
+    loader = SVGImportLoader()
+    svg_files = loader.load_svg_files_from_folder(folder_path)
     for svg_file in svg_files:
         glyph_name = parse_glyph_name(svg_file)  
         layer = convert_svg_to_glyphs_layer(svg_file)
