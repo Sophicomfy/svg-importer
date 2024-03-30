b7f75a1e821bbf5edb3fd5687b0d6de83cf4e0b1
filipaldi <piano.haggle.0d@icloud.com>
2024-03-30 13:13:05 +0100

    🥳 new parser is working as expected

--- amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_parser.py
@@ -1,10 +1,19 @@
-import xml.etree.ElementTree as ET
+import os
 
-def parse_glyph_name(svg_path):
-    file_name = svg_path.split('/')[-1]
-    if '_refined.svg' in file_name:
-        glyph_name = file_name.replace('_refined.svg', '')
+def parse_glyph_name(svg_file_path):
+    file_name = os.path.basename(svg_file_path)
+    parts = file_name.split('_')
+    if len(parts) >= 3 and parts[1].isdigit() and parts[2].isdigit():
+        glyph_name = '_'.join(parts[:2])
+        return glyph_name
     else:
-        glyph_name = file_name.replace('.svg', '')
-    print("Parsed Glyph Name:", glyph_name)
-    return glyph_name
+        return os.path.splitext(file_name)[0]
+
+def parse_layer_name(svg_file_path):
+    file_name = os.path.basename(svg_file_path)
+    parts = file_name.split('_')
+    if len(parts) >= 3 and parts[1].isdigit() and parts[2].isdigit():
+        layer_name = parts[2]
+        return layer_name
+    else:
+        return os.path.splitext(file_name)[0]
