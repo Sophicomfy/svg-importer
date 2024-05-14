0688ec5d81ab67f348a22cbbd4168ddcc96995b0
filipaldi <piano.haggle.0d@icloud.com>
2024-05-14 09:08:24 +0200

    🥳 Importing is working correctly

--- amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
+++ amicus.glyphsPlugin/Contents/Resources/svg_import_converter.py
@@ -1,16 +1,24 @@
-from GlyphsApp import GSLayer, GSSVGtoPath
+from GlyphsApp import GSLayer
 import objc
-from Foundation import NSURL
+from Foundation import NSURL, NSClassFromString
+GSSVGtoPath = NSClassFromString("GSSVGtoPath")
+
 
 def convert_svg_to_glyphs_layer(svg_file_path):
     layer = GSLayer()
     svg_to_path = GSSVGtoPath.alloc().init()
     url = NSURL.fileURLWithPath_(svg_file_path)
+
+    bounds = None
+    error = None
     
-    success = svg_to_path.readFile_toLayer_error_(url, layer, None)
+    success = svg_to_path.readFile_toLayer_bounds_error_(url, layer, bounds, error)
     if success:
-        print(layer)
+        print(f"SVG converted successfully for {svg_file_path}")
         return layer
     else:
-        print("Error during SVG conversion.")
+        if error:
+            print(f"Error during SVG conversion for {svg_file_path}: {error.localizedDescription()}")
+        else:
+            print(f"Error during SVG conversion for {svg_file_path}, but no error details were provided.")
         return None
