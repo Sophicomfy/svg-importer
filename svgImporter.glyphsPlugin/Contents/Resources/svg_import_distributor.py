9f4fcd6639b8c496330499e51b377963f10eb1a2
Filip <flp@Filips-MacBook-Pro.local>
2024-07-29 18:07:52 +0200

    🧹 move converter script to distributor

--- svgImporter.glyphsPlugin/Contents/Resources/svg_import_distributor.py
+++ svgImporter.glyphsPlugin/Contents/Resources/svg_import_distributor.py
@@ -1,7 +1,30 @@
+# svg_import_distributor.py
+
 import GlyphsApp
 from GlyphsApp import Glyphs, GSGlyph, GSLayer
-from svg_import_converter import convert_svg_to_glyphs_layer
+from Foundation import NSURL, NSClassFromString
 from svg_import_parser import parse_glyph_name, parse_layer_name
+
+GSSVGtoPath = NSClassFromString("GSSVGtoPath")
+
+def convert_svg_to_glyphs_layer(svg_file_path):
+    layer = GSLayer()
+    svg_to_path = GSSVGtoPath.alloc().init()
+    url = NSURL.fileURLWithPath_(svg_file_path)
+
+    bounds = None
+    error = None
+    
+    success = svg_to_path.readFile_toLayer_bounds_error_(url, layer, bounds, error)
+    if success:
+        print(f"SVG converted successfully for {svg_file_path}")
+        return layer
+    else:
+        if error:
+            print(f"Error during SVG conversion for {svg_file_path}: {error.localizedDescription()}")
+        else:
+            print(f"Error during SVG conversion for {svg_file_path}, but no error details were provided.")
+        return None
 
 def distribute_data(svg_file_path):
     font = Glyphs.font  # Access the current font
