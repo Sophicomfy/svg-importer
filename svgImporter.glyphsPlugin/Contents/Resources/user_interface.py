b85bc2759b1562b69f8dbc513a0ed8e722b8dbbf
Filip <flp@Filips-MacBook-Pro.local>
2024-07-29 18:06:51 +0200

    🧹 refactor user interface code

---
+++ svgImporter.glyphsPlugin/Contents/Resources/user_interface.py
@@ -0,0 +1,30 @@
+# user_interface.py
+
+from GlyphsApp import Glyphs, GetOpenFile, GetFolder
+from vanilla import Window, Button, TextBox
+from svg_import import selective_import_svg, batch_import_svgs
+
+class AmicusWindow:
+    def __init__(self):
+        self.w = Window((400, 160), "Amicus SVG Importer")
+        self.w.text = TextBox((15, 12, -15, 17), "Choose import option:", sizeStyle='small')
+        self.w.selectiveImportButton = Button((15, 40, 180, 20), "Selective Import", callback=self.selectiveImportCallback)
+        self.w.batchImportButton = Button((15, 70, 180, 20), "Batch Import", callback=self.batchImportCallback)
+        self.w.cancelButton = Button((15, 100, 180, 20), "Cancel", callback=self.closeWindow)
+        self.w.open()
+
+    def selectiveImportCallback(self, sender):
+        filePath = GetOpenFile("Select an SVG file")
+        if filePath:
+            selective_import_svg(filePath, GetFolder("Select a folder for name mappings"))
+
+    def batchImportCallback(self, sender):
+        folderPath = GetFolder("Select a folder containing SVG files")
+        if folderPath:
+            batch_import_svgs(folderPath)
+
+    def closeWindow(self, sender):
+        self.w.close()
+
+def showLTTRSVGImporterWindow():
+    AmicusWindow()
