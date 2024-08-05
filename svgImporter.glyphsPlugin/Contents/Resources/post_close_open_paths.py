cb39f89add0843a965de1a6584b51bea9812854e
Filip <flp@Filips-MacBook-Pro.local>
2024-08-05 15:25:14 +0200

    🎉 Fixed missing `distance` problem

--- svgImporter.glyphsPlugin/Contents/Resources/post_close_open_paths.py
+++ svgImporter.glyphsPlugin/Contents/Resources/post_close_open_paths.py
@@ -1,3 +1,8 @@
+import math
+
+def distance(point1, point2):
+    return math.sqrt((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2)
+
 def close_open_paths(font):
     for glyph in font.glyphs:
         for layer in glyph.layers:
