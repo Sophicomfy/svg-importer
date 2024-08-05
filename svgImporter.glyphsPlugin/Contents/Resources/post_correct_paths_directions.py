d84fd326ed66132e4a4ee0cc6002812bcb25711d
Filip <flp@Filips-MacBook-Pro.local>
2024-08-05 15:38:24 +0200

    🎉 repair `correctPathDirection()` function

--- svgImporter.glyphsPlugin/Contents/Resources/post_correct_paths_directions.py
+++ svgImporter.glyphsPlugin/Contents/Resources/post_correct_paths_directions.py
@@ -1,4 +1,4 @@
 def correct_paths_directions(font):
     for glyph in font.glyphs:
         for layer in glyph.layers:
-            layer.correctPathDirections()
+            layer.correctPathDirection()
