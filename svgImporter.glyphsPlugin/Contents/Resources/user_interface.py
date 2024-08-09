d31702957a3c607d15a11f1315314cd1566207aa
Filip <flp@Filips-MacBook-Pro.local>
2024-08-09 17:16:37 +0200

    UI vindow changes

--- svgImporter.glyphsPlugin/Contents/Resources/user_interface.py
+++ svgImporter.glyphsPlugin/Contents/Resources/user_interface.py
@@ -5,14 +5,18 @@ from post_names_conversion import run_name_conversion
 
 class AmicusWindow:
     def __init__(self):
-        self.w = Window((400, 200), "Amicus SVG Importer")
+        self.w = Window((400, 260), "Amicus SVG Importer")
+        
         self.w.text = TextBox((15, 12, -15, 17), "Choose import option:", sizeStyle='small')
         self.w.selectiveImportButton = Button((15, 40, 180, 20), "Selective Import", callback=self.selectiveImportCallback)
         self.w.batchImportButton = Button((15, 70, 180, 20), "Batch Import", callback=self.batchImportCallback)
         self.w.htmlImportButton = Button((15, 100, 180, 20), "HTML Import", callback=self.htmlImportCallback)
-        self.w.postProcessButton = Button((15, 130, 180, 20), "Post Process Paths", callback=self.postProcessCallback)
-        self.w.renameGlyphsButton = Button((15, 160, 180, 20), "Rename Glyphs", callback=self.renameGlyphsCallback)
-        self.w.cancelButton = Button((15, 190, 180, 20), "Cancel", callback=self.closeWindow)
+        
+        self.w.postProcessText = TextBox((15, 130, -15, 17), "Choose post-process option:", sizeStyle='small')
+        
+        self.w.postProcessButton = Button((15, 150, 180, 20), "Post Process Paths", callback=self.postProcessCallback)
+        self.w.renameGlyphsButton = Button((15, 180, 180, 20), "Rename Glyphs", callback=self.renameGlyphsCallback)
+        self.w.cancelButton = Button((15, 210, 180, 20), "Cancel", callback=self.closeWindow)
         self.w.open()
 
     def selectiveImportCallback(self, sender):
